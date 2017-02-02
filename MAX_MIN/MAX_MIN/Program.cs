3d3a7e7027ef182a7df48c084659b4de696815a4
aminaaman <aminaaman07@gmail.com>
2017-02-02 11:31:50 +0600

    Finding max and min in the file

---
+++ MAX_MIN/MAX_MIN/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO; //library for using StreamReader and StreamWriter
+
+namespace MAX_MIN
+{
+	class Program
+	{
+		static void Ex1()
+		{
+			StreamReader sr = new StreamReader(@"/Users/Adela/Documents/a.txt"); //showing the path
+			string[] arr = sr.ReadLine().Split(); // "1 3 5 7 9 11" -> arr[0] = "1", arr[1] = "3"...
+
+			int min = 1000000; //giving large number to check min
+			int max = 0; //giving small number to check min
+
+			foreach (string i in arr)    //converting array to string 
+			{
+				int a = int.Parse(i); //converting string to integer
+
+
+				//checks every number in array for minimum
+				if (a < min)
+				{
+					min = a;
+				}
+
+				//checks every number in array for maximum
+				if (a > max)
+				{
+					max = a;
+				}
+
+
+			}
+			Console.WriteLine("minimum number is " + min);
+			Console.WriteLine("maximum number is " + max);
+
+			sr.Close();
+		}
+		static void Main(string[] args)
+		{
+			Ex1();
+			Console.ReadKey();
+		}
+	}
+}
