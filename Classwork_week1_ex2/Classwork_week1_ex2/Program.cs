aeff5565f554fdcd27e97e8124a38bc20073fa2c
aminaaman <aminaaman07@gmail.com>
2017-02-14 19:45:10 +0600

    classwork examples

---
+++ Classwork_week1_ex2/Classwork_week1_ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Classwork_week1_ex2
+{
+	class MainClass
+	{
+		static bool isEven(int a)
+		{
+			if (a % 2 == 0)
+				return true;
+			return false;
+					
+		}
+
+		public static void Main(string[] args)
+		{
+			int n = args.Length;
+			for (int i = 0; i < n; i++) 
+			{
+				string s = args[i]; // "1"
+				int p = int.Parse(s); //p=5
+
+				if (isEven(p) == false)
+					Console.WriteLine(p);
+				Console.ReadKey();
+			}
+		}
+	}
+}
