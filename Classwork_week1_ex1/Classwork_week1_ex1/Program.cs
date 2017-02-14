103dc935f20a7b3031fb3178350ea8d5e80fef9c
aminaaman <aminaaman07@gmail.com>
2017-02-14 19:32:38 +0600

    classwork examples

---
+++ Classwork_week1_ex1/Classwork_week1_ex1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Classwork_week1_ex1
+{
+	class MainClass
+	{
+		class Student
+		{
+			public string name;
+			public string surname;
+			public int age;
+			public double gpa;
+
+			public Student(string name, string surname, int age)
+			{
+				this.name = name;
+				this.surname = surname;
+				this.age = age;
+			}
+
+			public double GetGPA()
+			{
+				return gpa;
+			}
+
+		}
+		public static void Main(string[] args)
+		{
+			//Student #1
+			Student a = new Student("Eldana", "Aman", 20);
+			a.gpa = 4.0;
+
+			//Student #2
+			Student b = new Student("Sholpan", "Agibetova", 21);
+			b.age = 21;
+			b.gpa = 3.9;
+
+			Console.WriteLine(a.name);
+			Console.WriteLine(b.name);
+			Console.ReadKey();
+
+		}
+	}
+}
